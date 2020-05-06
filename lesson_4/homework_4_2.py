e8781df65e36bc7ce66a2162048d723ab7950710
ingaprivet <inga.poznyak@gmail.com>
2020-05-05 22:05:43 -0400

    My message

--- lesson_4/homework_4_2.py
+++ lesson_4/homework_4_2.py
@@ -4,37 +4,46 @@
 # Write a function, which will calculate the difference of these two numbers
 
 def difference(num_1, num_2):
-    pass
+    if num_1 > num_2:
+        return  num_1 - num_2
+    else:
+        return num_2 - num_1
 
 
 # Division
 # Write a function, which will divide these two numbers
 
 def division(num_1, num_2):
-    pass
+    if num_1 > num_2:
+        return  num_1/num_2
+    else:
+        return num_2/num_1
 
 
-# Function gets random number. If this number is more than ten, return the difference between 100 and this number,
+# Function gets random number. If this number is more than ten,
+# return the difference between 100 and this number,
 # otherwise return this number multiplied by 10
 
 def function_1(number):
-    pass
+    if number > 10:
+        return 100 - number
+    else:
+        return number * 10
 
 
 # Your function temerature_convertor gets the temperature in Fahrenheit, convert it to Celsius and return.
 # Formula (32°F − 32) × 5/9 = 0°C
 
 def temerature_convertor(fahrenheit_degree):
-    pass
+    return (fahrenheit_degree - 32) * 5/9
 
 
 # Taxi Fare
-# In a particular jurisdiction, taxi fares consist of a base fare of $4.00, plus $0.25 for every 140 meters travelled.
-# Write a function that takes the distance travelled (in kilometers) as its only parameter and returns the total fare
+# In a particular jurisdiction, taxi fares consist of a base fare of $4.00,
+# plus $0.25 for every 140 meters travelled.
+# Write a function that takes the distance travelled (in kilometers) as
+# its only parameter and returns the total fare
 # as its only result rounded by 2 digits. Write a program that demonstrates the function.
 
 def taxi_fare(distance):
-    pass
-
-# examples of usage:
-# taxi_fare(10) #21.86
+    return round(distance * 1000 / 140 * 0.25 + 4, 2)

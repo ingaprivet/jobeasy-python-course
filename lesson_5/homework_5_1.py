b75a7fc8aa6185d51123f84b17f4392b20044385
ingaprivet <inga.poznyak@gmail.com>
2020-05-06 21:50:55 -0400

    My message

--- lesson_5/homework_5_1.py
+++ lesson_5/homework_5_1.py
@@ -24,24 +24,29 @@ def print_name_number_times(number_1, name_2):
     return name_2 * number_1
 
 
-
 # Enter a random string, which includes only digits.
 # Write function sum_digits which find a sum of digits in this string
 
-string_number_1 = '230967'
+string_number_1 = '123'
+
 
 def sum_digits(string_number_1):
+    result = 0
     for char in string_number_1:
-        return int(char) + int(char)
+        result = result + int(char)
+    return result
 
+
+print(sum_digits('123'))
 
 
 # Create function which sums up all even numbers between 2 and 100 (include 100)
 
 def sum_even_numbers():
+    result = 0
     for number in range(2, 101):
+        if (number % 2) == 0:
+            result = result + number
+    return result
 
-         if (number % 2 == 0):
-
-                return number + number
-
+print(sum_even_numbers())
